82e357c1fa32f3fdee5c08630f35c4a3eec2c5c7
d.zalega <daniel.zalega@gmail.com>
2018-09-16 13:01:13 +0200

    Splitting functions that are meant to populate levels into utils file

---
+++ source/game_loop.hpp
@@ -0,0 +1,22 @@
+//
+// Created by xdbeef on 25.02.18.
+//
+
+#ifndef SPELUNKYDS_SCROLLING_H
+#define SPELUNKYDS_SCROLLING_H
+
+#include <nds/arm9/sprite.h>
+#include "tiles/level.hpp"
+#include "sprites/sprite_info.h"
+#include "sprites/collectibles/moniez.hpp"
+#include "globals_declarations.hpp"
+#include "sprites/enemies/snake.hpp"
+#include "sprites/items/jar.hpp"
+#include "sprites/items/rock.hpp"
+
+namespace gameloop {
+
+    void scroll();
+
+}
+#endif //SPELUNKYDS_SCROLLING_H
---
+++ source/tiles/populating_utils.hpp
@@ -0,0 +1,14 @@
+//
+// Created by xdbeef on 16.09.18.
+//
+
+#ifndef SPELUNKYDS_POPULATING_UTILS_H
+#define SPELUNKYDS_POPULATING_UTILS_H
+
+void populate_cave_npcs();
+
+void populate_cave_moniez();
+
+void populate_main_menu();
+
+#endif //SPELUNKYDS_POPULATING_UTILS_H
